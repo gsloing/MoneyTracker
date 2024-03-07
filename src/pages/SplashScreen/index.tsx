bca1504aee6aa73a57fc7894b33db1b9ab96d0f5
gsloing <gabysloing@gmail.com>
2024-03-07 17:53:27 +0800

    adding react navigation, sign in page, refactor router and pages

--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,8 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Logo from '../../assets/icon/Logo.svg';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Logo />
@@ -14,14 +20,14 @@ const SplashScreen = () => {
 export default SplashScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#02CF8E',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 32,
-        fontFamily: 'Poppins-Medium',
-    },
+  container: {
+    backgroundColor: '#02CF8E',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-Medium',
+  },
 });
