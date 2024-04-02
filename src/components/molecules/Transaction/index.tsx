e1bfb4b2ff9311d28238aa150f99fd85e9ceda61
gsloing <gabysloing@gmail.com>
2024-04-02 15:03:49 +0800

    fixing error

--- src/components/molecules/Transaction/index.tsx
+++ src/components/molecules/Transaction/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Gap } from '../..'
 
 const Transaction = ({label, date, type, cash}) => {
   const cashColor = cash.includes('-') ? 'red' : 'green';
